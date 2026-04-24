382172e1e8aedcfe55c2279c972aca801e4bc772
Manoel Carvalho <manoelmecat@gmail.com>
2026-04-24 16:28:08 -0300

    feat: acesso as paginas somente com login

--- partials/header.php
+++ partials/header.php
@@ -5,19 +5,23 @@ require_once "../funcoes/retornaAsIniciais.php";
     <div>
         <span>
             <?php
+            echo '<a href="../uploads.php">';
             if ($_SESSION['nome']) {
                 if ($_SESSION['avatar']) {
-                    echo "<img style='width: 30px; height: 30px;border: 1px solid #ccc; border-radius: 50%; padding: 5px' src='../uploads/".$_SESSION['avatar']."'>";
+                    echo "<img style='width: 30px; height: 30px;border: 1px solid #ccc; border-radius: 50%; padding: 5px' src='../uploads/" . $_SESSION['avatar'] . "'>";
                 } else {
                     echo "<span style='width: 30px; height: 30px;border: 1px solid #ccc; border-radius: 50%; padding: 5px'>";
                     echo RetornaIniciais($_SESSION['nome']);
                     echo "</span>";
                 }
-
+                echo '</a>';
                 echo "Olá, Bem vindo, " . $_SESSION['nome'];
             }
             ?>
         </span>
+
+
+        </a>
         <a href="../funcoes/sairLogin.php">Sair</a>
     </div>
 </header>
--- uploads.php
+++ uploads.php
@@ -1,36 +1,25 @@
 <?php
-    session_start();
+session_start();
+
+if (!isset($_SESSION['isLogged'])) {
+    header('location: ../login.php');
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="salvar.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="file">
         <button type="submit">Enviar arquivo</button>
     </form>
-
-    <hr>
-
-    <h3>Uploads</h3>
-        
-    <?php
-        require_once('funcoes/verificaTipoDoArquivo.php');
-        $arquivos = scandir('uploads');
-        foreach($arquivos as $arquivo){
-            if(!($arquivo == "." || $arquivo == "..") ){
-                VerificaTipoDoArquivo($arquivo);
-            }    
-        }
-
-        
-    ?>
-
-
-
 </body>
+
 </html>
